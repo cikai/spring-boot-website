ee177329379317b4a1e71ecb26fd845264347266
cikai <cikai0923@gmail.com>
2018-01-01 14:08:32 +0800

    add note page, redesign index page

--- src/main/java/me/cikai/website/controller/AppController.java
+++ src/main/java/me/cikai/website/controller/AppController.java
@@ -1,15 +1,8 @@
 package me.cikai.website.controller;
 
-import me.cikai.website.model.AliyunEcsFee;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 /**
  * @author cikai
@@ -18,21 +11,8 @@ import java.util.List;
 @RequestMapping(path = "")
 public class AppController {
 
-  @Autowired
-  JdbcTemplate jdbcTemplate;
-
   @GetMapping(path = "")
-  public String index(Model model) {
-    List aliyunEcsFeeList = (List) jdbcTemplate.query("SELECT `id`, `date`, `fee` FROM `aliyun-ecs-fee`",
-      (RowMapper) (rs, rowNumber) -> {
-        AliyunEcsFee aliyunEcsFee = new AliyunEcsFee();
-        aliyunEcsFee.setId(rs.getInt("id"));
-        aliyunEcsFee.setDate(rs.getString("date"));
-        aliyunEcsFee.setFee(rs.getInt("fee"));
-        return aliyunEcsFee;
-      }
-    );
-    model.addAttribute("alifees", aliyunEcsFeeList);
+  public String index() {
     return "index";
   }
 }
